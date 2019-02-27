3c73d054c8fa25f8688a1f12dab28a4cf7154f0c
peterobama <46796612+peterobama@users.noreply.github.com>
2019-02-27 11:56:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function parallaxIt(e, target, movement) {
     y: (relY - $this.height() / 2) / $this.height() * movement
   });
 }
+
+$(window).mousemove(function(e) {
+    var change;
+    var xpos=e.clientX;var ypos=e.clientY;var left= change*20;
+    var  xpos=xpos*2;ypos=ypos*2;
+    $('.man').css('top',((0+(ypos/50))+"px"));
+    $('.man').css('right',(( 0+(xpos/80))+"px"));
+                   
+  });
